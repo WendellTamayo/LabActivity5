8efde5622b48a3a1c007ef0400ff8224c91914fc
WendellTamayo <107296776+WendellTamayo@users.noreply.github.com>
2022-12-14 15:07:34 +0800

    Complete SimpleCalculator

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,15 +1,44 @@
 package SimpleCalc;
 
 import javax.swing.*;
-import javax.swing.border.Border;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
-    private JPanel tfNumber2;
+    private JPanel JPanel;
     private JTextField lblResult;
+    private JTextField tfNumber2;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
+                String operation = cbOperations.getSelectedItem().toString();
+                int result = 0;
+                switch (operation) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                }
+                lblResult.setText(String.valueOf(result));
+            }
+        });
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI calc = new SimpleCalcGUI();
